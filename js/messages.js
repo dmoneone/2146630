e537827646960299cfaa610e219b70de388264c6
dmitry <kirildim16@gmail.com>
2019-07-18 16:00:44 +0300

    fix valid system

--- js/messages.js
+++ js/messages.js
@@ -1,31 +1,35 @@
 $(document).ready(function(){
    let flag = false;
    
-   function inputValidate(value){
-       if ( value.length > 3 ) return true;
-       else return false;
+   function isInputInvalid(value){
+       if ( value.length > 3 ) {
+          $('#in').css('border','1px solid #a9a9a9');
+          $('.error').hide();
+          $('#in').val('');
+          return false;
+       }
+       else{
+          $('#in').css('border','1px solid red');
+          $('.error').show();
+          $('#in').val('');
+          return true;
+       }
    }
     
    function addComment(){
        let input_value = $('#in').val();
-       if ( inputValidate(input_value) == true ) {
+       if ( isInputInvalid(input_value) ) 
+          return;
+
           let comment = {
              time: Math.floor(Date.now()/1000),
              message: input_value
           } 
           addCommentToDOM(comment);
-          $('#in').val('');
-          $('#in').css('border','1px solid #a9a9a9');
-          $('.error').hide();
-       }
-       else{
-           $('#in').css('border','1px solid red');
-           $('.error').show();
-       }
-           
+       
    }
     
-   function addCommentToDOM(obj) {
+   function addCommentToDOM(obj){
        $('<div/>',{class: "review-block"}).append($('<span/>',{class: "reviewer-name", text: "NO NAME"}), $('<span/>',{class: "time", text: timeConverter(obj.time)}), $('<div/>',{class: "message-wrap"}).append($('<div/>',{class: "triangle"}).append($('<img/>',{src: "img/tr.png"})), $('<p/>',{ text: obj.message}))).appendTo($('.review-block-wrap'));
    }
                                                                                                             
